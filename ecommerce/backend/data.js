1fa6e662da7ea15629b4a24ed74d1cbd6c6a9929
IamPratikSinha <iampratikkumarsinha@gmail.com>
2022-10-12 13:47:31 +0530

    Fetch Product commit

---
+++ ecommerce/backend/data.js
@@ -0,0 +1,47 @@
+const data = {
+  products: [
+    {
+      id: 100001,
+      name: 'Nike Slim shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.3,
+      numReviews: 15,
+      description: 'high quality shirt',
+    },
+
+    {
+      id: 100002,
+      name: 'Nike Slim Pant',
+      slug: 'nike-slim-pant',
+      category: 'Pants',
+      image: '/images/p2.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.3,
+      numReviews: 15,
+      description: 'high quality Pant',
+    },
+
+    {
+      id: 100003,
+      name: 'Addidas Slim shirt',
+      slug: 'addidas-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p3.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Addidas',
+      rating: 4.3,
+      numReviews: 15,
+      description: 'high quality shirt',
+    },
+  ],
+};
+
+export default data;
--- ecommerce/frontend/src/screens/HomeScreen.js
+++ ecommerce/frontend/src/screens/HomeScreen.js
@@ -1,9 +1,19 @@
-import data from './../data';
+// import data from './../data';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
   return (
     <div className="products">
-      {data.products.map((product) => (
+      {products.map((product) => (
         <div className="product" key={product.id}>
           <Link to={`/product/${product.slug}`}>
             <img src={product.image} alt={product.name} />
