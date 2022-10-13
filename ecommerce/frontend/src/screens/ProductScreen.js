dbf57e3470e7bce8877f4c3c8855dd1c019ba134
IamPratikSinha <iampratikkumarsinha@gmail.com>
2022-10-13 16:52:12 +0530

    Loading and error component commit

--- ecommerce/frontend/src/screens/ProductScreen.js
+++ ecommerce/frontend/src/screens/ProductScreen.js
@@ -12,6 +12,9 @@ import Button from 'react-bootstrap/Button';
 
 import Product from './../Components/Product';
 // import { Button } from 'react-bootstrap/Button';
+import LoadingBox from './../Components/LoadingBox';
+import MessageBox from './../Components/MessageBox';
+import { getError } from './../utils';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -41,16 +44,16 @@ function ProductScreen() {
         const result = await axios.get(`/api/products/slug/${slug}`);
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
       }
     };
     fetchData();
   }, [slug]);
 
   return loading ? (
-    <div>Loading...</div>
+    <LoadingBox />
   ) : error ? (
-    <div>{error}</div>
+    <MessageBox variant="danger">{error}</MessageBox>
   ) : (
     <div>
       <Row>
